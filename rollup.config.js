262f70be6ffa3e1e90fd8d542cb0e37c4c1cc323
David Sargeant <admin@sesaonsite.com>
2022-08-16 17:26:56 -0500

    Almost working

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
 import isBuiltin from 'is-builtin-module';
 import babel from 'rollup-plugin-babel';
+import commonJs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import nodeBuiltins from 'rollup-plugin-node-builtins';
+import nodeGlobals from 'rollup-plugin-node-globals';
+import nodePolyfill from 'rollup-plugin-polyfill-node';
 import packageJson from './package.json';
 import tweakDefault from './build/rollup-plugin';
 
@@ -13,12 +18,32 @@ export default {
     { file: 'lib/index.js', format: 'cjs', exports: 'named' },
     { file: 'lib/index.es.js', format: 'es', exports: 'named', intro: 'process.emitWarning("The .es.js file is deprecated. Use .mjs instead.");' },
     { file: 'lib/index.mjs', format: 'es', exports: 'named' },
+    { file: 'lib/index.browser.js', browser: true, format: 'iife', sourceMap: 'inline', name: "nodeFetch", exports: 'named' },
+    { file: 'lib/index.browser.es.js', format: 'esm', exports: 'named' },
   ],
   plugins: [
-    babel({
-      runtimeHelpers: true
+    nodeResolve({
+      // mainFields: [
+      //   'main',
+      //   'module',
+      //   'browser',
+      // ],
+      "jsnext": true,
+      "main": true,
+      "browser": true,
+      preferBuiltins: true,
     }),
-    tweakDefault()
+    nodeGlobals(),
+    // nodePolyfill(),
+    // nodeBuiltins(),
+    babel({
+      runtimeHelpers: true,
+      
+    }),
+    commonJs({
+      include: /node_modules/
+    }),
+    tweakDefault(),
   ],
   external: function (id) {
     return dependencies.includes(id) || isBuiltin(id);
